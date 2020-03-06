83814a8973f92b24e044eec91f23a74b8dc4486a
Luigui665 <latombe8@misena.edu.co>
2020-03-06 18:51:56 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+//Base de datos
+let irlDB;
+
+if (process.env.NODE_ENV === "dev") {
+    urlDB = "mongodb://localhost:27017/cafe";
+} else {
+    urlDB =
+        "mongodb+srv://Huntenatorem:YuFMgtQMriL1JHdc@cluster0-zocby.mongodb.net/cafe";
+}
+
+process.env.urlDB = urlDB;
