d252337cdba9b47f521c3572d83ce4a0d6c606ff
Luigui665 <latombe8@misena.edu.co>
2020-03-07 18:27:57 -0500

    Login normal

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,9 +2,15 @@ const express = require("express");
 //const bcrypt = require("bcrypt");
 const _ = require("underscore");
 const Usuario = require("../models/usuario");
+
+const {
+    verificaToken,
+    verificaAdmin_Role
+} = require("../middlewares/autenticacion");
+
 const app = express();
 
-app.get("/usuario", function(req, res) {
+app.get("/usuario", verificaToken, (req, res) => {
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
@@ -34,7 +40,7 @@ app.get("/usuario", function(req, res) {
         });
 });
 
-app.post("/usuario", function(req, res) {
+app.post("/usuario", [verificaToken, verificaAdmin_Role], function(req, res) {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -62,7 +68,10 @@ app.post("/usuario", function(req, res) {
     });
 });
 
-app.put("/usuario/:id", function(req, res) {
+app.put("/usuario/:id", [verificaToken, verificaAdmin_Role], function(
+    req,
+    res
+) {
     let id = req.params.id;
     let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
@@ -111,7 +120,10 @@ app.put("/usuario/:id", function(req, res) {
 //     });
 // });
 
-app.delete("/usuario/:id", function(req, res) {
+app.delete("/usuario/:id", [verificaToken, verificaAdmin_Role], function(
+    req,
+    res
+) {
     let id = req.params.id;
 
     let cambiaEstado = {
